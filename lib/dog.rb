be59127548f78930759161950dd4a1fe4114dfe5
Michael Lynch <michael@michaelrlynch.com>
2019-11-04 03:01:23 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,10 +1,10 @@
 class Dog
   attr_accessor :name, :breed, :id
 
-  def initialize(id = nil, name, breed)
+  def initialize(id: nil, name:, breed:)
     @name = name
     @breed = breed
-
+    @id = id
   end
 
 end
